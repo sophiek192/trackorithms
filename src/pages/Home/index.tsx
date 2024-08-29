3c9b5a800f6f782eeb983400aa316957bd0cd5df
root <root@Ricky-pc>
2024-08-29 20:47:17 +1000

    I play pokemon go everyday

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,31 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Link to="/1">Game</Link>
-    </div>
+    <> 
+      <link rel = "stylesheet" href="home.css"/> 
+      <header className = "colorful-header">
+        <div className = "blue-band"></div>
+        <div className = "red-band"></div>
+        <div className = "yellow-band"></div> 
+        <p className = "title">Trackorithms</p>
+      </header> 
+      <div>
+          <img className ="main-photo" src="train3.png"/>
+      </div>
+      <div className ="main-grid">
+          <button className = "leader-board">
+              LEADER BOARD
+              <img className ="leader-photo" src="leader3.png"/>
+          </button>  
+          <button className = "play">
+              PLAY 
+          </button>  
+          <button className = "how-to">
+              TUTORIAL 
+              <img className ="tut-photo" src="tutorial-photo.png"/>
+          </button> 
+      </div> 
+    </>
   );
 }
