54f9ab8a0b8816676518002e1a61b441cdd8622c
Josh <joshconstantinelim@gmail.com>
2024-08-30 11:34:30 +1000

    edit link of frontend station to go to sophies page

--- src/pages/frontend-station/index.tsx
+++ src/pages/frontend-station/index.tsx
@@ -1,37 +1,37 @@
-import { useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './style.css';
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
 
 export default function Home() {
   const containerRefFrontEndStation = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     // Fetch the HTML file and insert it into the component
-    fetch('./src/pages/Home/frontendStation.html') // Adjust the path based on your setup
+    fetch("./src/pages/Home/frontendStation.html") // Adjust the path based on your setup
       .then((response) => response.text())
       .then((html) => {
         if (containerRefFrontEndStation.current) {
           containerRefFrontEndStation.current.innerHTML = html;
         }
       })
-      .catch((error) => console.error('Error loading HTML:', error));
+      .catch((error) => console.error("Error loading HTML:", error));
   }, []);
 
   const navigate = useNavigate();
 
   const handleLeave = () => {
     // Trigger the train leave animation
-    const trainElement = document.getElementById('Train');
+    const trainElement = document.getElementById("Train");
     if (trainElement) {
       // Start the train leave animation
-      trainElement.style.animation = 'trainAnimationLeave 5s ease-in forwards';
+      trainElement.style.animation = "trainAnimationLeave 5s ease-in forwards";
 
       // Listen for the end of the animation to navigate to a new page
       trainElement.addEventListener(
-        'animationend',
+        "animationend",
         () => {
           // Navigate to the new page after the train has left
-          navigate('/frontend-station'); // Update with the correct route or use window.location.href
+          navigate("/modules/security"); // Update with the correct route or use window.location.href
         },
         { once: true } // Ensure the listener is called only once
       );
